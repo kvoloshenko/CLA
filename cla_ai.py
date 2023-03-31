84cfebf866a7442f25a2099abfdddb0c6ddabbb1
kvoloshenko <kvoloshenko@gmail.com>
2023-03-31 16:58:56 +0300

    Refactoring

--- cla_ai.py
+++ cla_ai.py
@@ -1,24 +1,19 @@
-# Работа с массивами данных
 import numpy as np
 from tensorflow.keras.models import model_from_json
-# Функции-утилиты для работы с категориальными данными
 from tensorflow.keras import utils
-# Масштабирование данных
 from sklearn.preprocessing import StandardScaler
 import joblib
 
-# Для нормализации данных используется готовый инструмент
 y_scaler = StandardScaler()
 y_scaler = joblib.load('std_scaler.bin')
 
 MAX_NUMBER_LINES = 300
 
-# Классы CUSTOMER_TYPE
 CUSTOMER_TYPE_class = [0,
                     {'Business Customer Account	'  : 0,
                      'Residential Customer Account': 1
                     }]
-# Классы CUSTOMER_SUB_SEGMENT
+
 CUSTOMER_SUB_SEGMENT_class = [0,
               {'Fixed & Mobile Expat' : 0,
                'Mobile Expat'         : 1,
@@ -28,7 +23,6 @@ CUSTOMER_SUB_SEGMENT_class = [0,
                'Fixed UAE'            : 5
                              }]
 
-# Вычисление счетчиков для данных разбиения
 for class_desc in [CUSTOMER_TYPE_class,
                    CUSTOMER_SUB_SEGMENT_class]:
     if isinstance(class_desc[1], list):
@@ -37,14 +31,9 @@ for class_desc in [CUSTOMER_TYPE_class,
         class_desc[0] = max(class_desc[1].values()) + 1
 
 def str_to_multi(arg, class_dict):
-    # Определение размерности выходного вектора
     num_classes = class_dict[0]
-
-    # Создание нулевого вектора
     result = np.zeros(num_classes)
 
-    # Поиск значения в словаре и, если найдено,
-    # выставление 1. на нужной позиции
     for value, cls in class_dict[1].items():
         # print(f'value={value} cls={cls}')
         if value == arg:
@@ -63,14 +52,13 @@ def extract_NUMBER_LINES_Category(arg, MAX_NUMBER_LINES):
   return NUMBER_LINES
 class ClaAi:
     def __init__(self):
-        # Открываем json файл разметки модели
         json_file = open('model.json', 'r')
-        loaded_model_json = json_file.read()  # считываем
-        json_file.close()  # закрываем
+        loaded_model_json = json_file.read()
+        json_file.close()
         self.loaded_model = model_from_json(loaded_model_json)  # используем керас, чтобы считать разметку архитектуры
         self.loaded_model.summary()
-        self.loaded_model.load_weights("model.h5")  # подгружаем веса
-        print("Загружено с диска")
+        self.loaded_model.load_weights("model.h5")
+        print("Model loaded from disk")
 
     def data_preparation(self, NUMBER_LINES, CUSTOMER_TYPE, CUSTOMER_SUB_SEGMENT):
         NUMBER_LINES_vec = extract_NUMBER_LINES_Category(NUMBER_LINES, MAX_NUMBER_LINES)
